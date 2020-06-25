c084cb992a2e83e7e5087cd2cafefca4517aab90
gubmax <gubyshev@gmail.com>
2020-06-25 22:03:50 +0300

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
+import { ConfigModule } from '@nestjs/config'
+
+import { AppController } from 'app.controller'
+import configuration from 'config/env.config'
 
 @Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration],
+  })],
   controllers: [AppController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
-import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+
+import { AppModule } from 'app.module'
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
+    AppModule, new FastifyAdapter(),
   )
+  const config = app.get(ConfigService)
+  const port = config.get<number>('port')
 
-  await app.listen(3000)
+  await app.listen(port!)
 }
 
 bootstrap()
